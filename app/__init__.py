94695601856c1389dc4598cdb761c1d1f6b5aca8
danielcopelin <danielcopelin@gmail.com>
2019-06-25 22:19:21 +1000

    Working example Dash app added

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
+import dash
 from flask import Flask, request, current_app
+from flask.helpers import get_root_path
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
+from flask_login import LoginManager, login_required
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from config import Config
@@ -12,8 +14,8 @@ from config import Config
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
-login.login_view = 'auth.login'
-login.login_message = 'Please log in to access this page.'
+login.login_view = "auth.login"
+login.login_message = "Please log in to access this page."
 mail = Mail()
 bootstrap = Bootstrap()
 
@@ -22,6 +24,8 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    register_dashapps(app)
+
     db.init_app(app)
     migrate.init_app(app, db)
     login.init_app(app)
@@ -29,50 +33,93 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
 
     from app.errors import bp as errors_bp
+
     app.register_blueprint(errors_bp)
 
     from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
 
     from app.main import bp as main_bp
+
     app.register_blueprint(main_bp)
 
     if not app.debug and not app.testing:
-        if app.config['MAIL_SERVER']:
+        if app.config["MAIL_SERVER"]:
             auth = None
-            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-                auth = (app.config['MAIL_USERNAME'],
-                        app.config['MAIL_PASSWORD'])
+            if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+                auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
             secure = None
-            if app.config['MAIL_USE_TLS']:
+            if app.config["MAIL_USE_TLS"]:
                 secure = ()
             mail_handler = SMTPHandler(
-                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-                toaddrs=app.config['ADMINS'], subject='Dacy Budget Failure',
-                credentials=auth, secure=secure)
+                mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+                fromaddr="no-reply@" + app.config["MAIL_SERVER"],
+                toaddrs=app.config["ADMINS"],
+                subject="Dacy Budget Failure",
+                credentials=auth,
+                secure=secure,
+            )
             mail_handler.setLevel(logging.ERROR)
             app.logger.addHandler(mail_handler)
 
-        if app.config['LOG_TO_STDOUT']:
+        if app.config["LOG_TO_STDOUT"]:
             stream_handler = logging.StreamHandler()
             stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
         else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/dacybudget.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
+            if not os.path.exists("logs"):
+                os.mkdir("logs")
+            file_handler = RotatingFileHandler(
+                "logs/dacybudget.log", maxBytes=10240, backupCount=10
+            )
+            file_handler.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s %(levelname)s: %(message)s "
+                    "[in %(pathname)s:%(lineno)d]"
+                )
+            )
             file_handler.setLevel(logging.INFO)
             app.logger.addHandler(file_handler)
 
         app.logger.setLevel(logging.INFO)
-        app.logger.info('Dacy Budget startup')
+        app.logger.info("Dacy Budget startup")
 
     return app
+
+
+def register_dashapps(app):
+    from app.dashapp1.layout import layout
+    from app.dashapp1.callbacks import register_callbacks
+
+    # Meta tags for viewport responsiveness
+    meta_viewport = {
+        "name": "viewport",
+        "content": "width=device-width, initial-scale=1, shrink-to-fit=no",
+    }
+    external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+    dashapp1 = dash.Dash(
+        __name__,
+        server=app,
+        url_base_pathname="/dashboard/",
+        assets_folder=get_root_path(__name__) + "/dashboard/assets/",
+        meta_tags=[meta_viewport],
+        external_stylesheets=external_stylesheets,
+    )
+
+    dashapp1.title = "Dashapp 1"
+    dashapp1.layout = layout
+    dashapp1.url_base_pathname = "/dashboard/"  # I dont know why I had to do this
+    register_callbacks(dashapp1)
+    _protect_dashviews(dashapp1)
+
+
+def _protect_dashviews(dashapp):
+    for view_func in dashapp.server.view_functions:
+        if view_func.startswith(dashapp.url_base_pathname):
+            dashapp.server.view_functions[view_func] = login_required(
+                dashapp.server.view_functions[view_func]
+            )
 
 
 from app import models
---
+++ app/dashapp1/layout.py
@@ -0,0 +1,22 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+layout = html.Div(
+    [
+        html.H1("Stock Ticker"),
+        html.A("Home", href="../index"),
+        html.Br(),
+        html.A("Logout", href="../auth/logout"),
+        dcc.Dropdown(
+            id="my-dropdown",
+            options=[
+                {"label": "Coke", "value": "COKE"},
+                {"label": "Tesla", "value": "TSLA"},
+                {"label": "Apple", "value": "AAPL"},
+            ],
+            value="COKE",
+        ),
+        dcc.Graph(id="my-graph"),
+    ],
+    style={"width": "500"},
+)
