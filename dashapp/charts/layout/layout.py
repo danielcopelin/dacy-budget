84a2346116f242360e51114d565d02708d6fdec0
danielcopelin <danielcopelin@gmail.com>
2019-07-06 11:45:32 +1000

    Added first test chart

--- dashapp/charts/layout/layout.py
+++ dashapp/charts/layout/layout.py
@@ -1,9 +1,8 @@
+import calendar
 import dash_core_components as dcc
 import dash_html_components as html
-
-# import dash_table
-# import dash_table.FormatTemplate as FormatTemplate
 import pandas as pd
+import plotly.graph_objs as go
 
 
 def layout(app):
@@ -14,8 +13,32 @@ def layout(app):
         transactions = db.session.query(Transaction)
         df = pd.read_sql(transactions.statement, transactions.session.bind)
 
-        accounts = Transaction.query.with_entities(Transaction.account).distinct()
+    df = df[(df.amount < 0) & (df.category != "Credit Card")]
+    df_grouped = df.groupby(df.date.dt.month).agg({"amount": "sum"})
 
-    layout = html.Div(["charts will go here"])
+    layout = html.Div(
+        children=[
+            dcc.Graph(
+                id="expenses_chart",
+                figure={
+                    "data": [go.Bar(x=df_grouped.index, y=df_grouped.amount * -1)],
+                    "layout": go.Layout(
+                        xaxis={
+                            "title": "Month",
+                            "tickmode": "array",
+                            "tickvals": df_grouped.index,
+                            "ticktext": df_grouped.index.to_series().apply(
+                                lambda x: calendar.month_name[x]
+                            ),
+                        },
+                        yaxis={"title": "Expenses $"},
+                        margin={"t": 10},
+                        hovermode="closest",
+                        barmode="stack",
+                    ),
+                },
+            )
+        ]
+    )
 
     return layout
